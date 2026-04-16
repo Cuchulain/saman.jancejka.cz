d85c6f70443352ca6ae2ea5d91a01358b0d2d260
Jan Čejka <posta@jancejka.cz>
2026-04-16 22:05:01 +0200

    Aktualizace domény na jancejka.cz a změna faviconu na šamanský buben.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'static', // Explicitly specify static output (which is also the default)
-  site: 'https://saman.jancejka.cz',
+  site: 'https://jancejka.cz',
   // Enable built-in image optimization with specific settings
   image: {
     service: {
